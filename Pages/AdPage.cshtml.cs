c0f3d81009f79b90ff81d3915833d779a53419c9
jsirugo <jsirugo@yahoo.com>
2024-05-06 16:06:26 +0200

    Added more pages
    "myaccountpage" and "AdPage" added

---
+++ Pages/AdPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FogelFormedlingenAB.Pages
+{
+    public class AdPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
