7c2ed7eb26bee0eb4ea3dc53cadce434a62d713c
Axelskold93 <142592414+Axelskold93@users.noreply.github.com>
2024-05-07 10:58:19 +0200

    Started adding sampledata ads, and started building display of ads.

--- Data/SampleData.cs
+++ Data/SampleData.cs
@@ -32,5 +32,35 @@ namespace FogelFormedlingenAB.Data
 
             database.SaveChanges();
         }
+        public static void CreateAds(AppDbContext database)
+        {
+            if(!database.Ads.Any())
+            {
+                database.Ads.Add(new Ad
+                {
+                    PictureUrl = "fiskmås-svante.jpg",
+                    Description = "En fin mås jag haft i trädgården. Lyssnar till namnet Svante och äter endast sardiner. Cirka 3 år gammal. Fint skick. BVSA!",
+                    Category = "Svensk Fågel",
+                    IsActive = true,
+                    AccountID = 1,
+                    Price = 499,
+                    Title = "3 årig fin fiskmås!",
+                    StartDate = "2024-05-06",
+                    EndDate = "2024-06-06"
+                });
+				database.Ads.Add(new Ad
+				{
+					PictureUrl = "marabou-sven.jpg",
+					Description = "Dags att sälja min pärla! Sven är en 5-årig marabou-stork. Van vid barn. Säljes pga ändrade livsförhållanden!!",
+					Category = "Utländsk Fågel",
+					IsActive = true,
+					AccountID = 2,
+					Price = 700,
+					Title = "Marabou-Stork söker nytt hem",
+					StartDate = "2024-05-06",
+					EndDate = "2024-06-06"
+				});
+			}
+        }
     }
 }
