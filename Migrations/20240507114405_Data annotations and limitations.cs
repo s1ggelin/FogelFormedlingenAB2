d892538ee4b2633d2fa727b0c49c1e6d00ccdd60
JoakimHaglund <joakimhaglund@outlook.com>
2024-05-07 13:59:28 +0200

    Added migration

---
+++ Migrations/20240507114405_Data annotations and limitations.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FogelFormedlingenAB.Migrations
+{
+    public partial class Dataannotationsandlimitations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Ads");
+
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Ads");
+
+            migrationBuilder.DropColumn(
+                name: "PictureUrl",
+                table: "Ads");
+
+            migrationBuilder.RenameColumn(
+                name: "SellerID",
+                table: "Ads",
+                newName: "AccountID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "reports",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BoughtDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PictureUrl",
+                table: "Images",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Ads",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "Ads",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Ads",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PictureID",
+                table: "Ads",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryID",
+                table: "Ads",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Accounts",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Accounts",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phonenumber",
+                table: "Accounts",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_MyEntity_OneColumnOnly",
+                table: "reports",
+                sql: "(AccountID IS NOT NULL AND AdID IS NULL) OR (AccountID IS NULL AND AdID IS NOT NULL)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_MyEntity_OneColumnOnly",
+                table: "reports");
+
+            migrationBuilder.DropColumn(
+                name: "BoughtDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "AccountID",
+                table: "Ads");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryID",
+                table: "Ads");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "Phonenumber",
+                table: "Accounts");
+
+			migrationBuilder.DropColumn(
+				name: "PictureID",
+				table: "Ads");
+
+			migrationBuilder.RenameColumn(
+                name: "AccountID",
+                table: "Ads",
+                newName: "SellerID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "reports",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PictureUrl",
+                table: "Images",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Ads",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StartDate",
+                table: "Ads",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Ads",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Ads",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EndDate",
+                table: "Ads",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PictureUrl",
+                table: "Ads",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+        }
+    }
+}
