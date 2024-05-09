aec666570e619f11e40aa4210d848df5cab50101
JoakimHaglund <joakimhaglund@outlook.com>
2024-05-09 13:11:00 +0200

    Created new clean migation

---
+++ Migrations/20240509110643_Updated database clean migration.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FogelFormedlingenAB.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updateddatabasecleanmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OpenIDIssuer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OpenIDSubject = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Phonenumber = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categories", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PictureUrl = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    AdID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ads",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountID = table.Column<int>(type: "int", nullable: false),
+                    PictureID = table.Column<int>(type: "int", nullable: false),
+                    CategoryID = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Price = table.Column<decimal>(type: "money", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ads", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Ads_Accounts_AccountID",
+                        column: x => x.AccountID,
+                        principalTable: "Accounts",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ads_Images_PictureID",
+                        column: x => x.PictureID,
+                        principalTable: "Images",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ads_categories_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "categories",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Favourites",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountID = table.Column<int>(type: "int", nullable: false),
+                    AdID = table.Column<int>(type: "int", nullable: false),
+                    AdID1 = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Favourites", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Favourites_Accounts_AccountID",
+                        column: x => x.AccountID,
+                        principalTable: "Accounts",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Favourites_Ads_AdID",
+                        column: x => x.AdID,
+                        principalTable: "Ads",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Favourites_Ads_AdID1",
+                        column: x => x.AdID1,
+                        principalTable: "Ads",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountId = table.Column<int>(type: "int", nullable: false),
+                    AdID = table.Column<int>(type: "int", nullable: false),
+                    BoughtDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AdID1 = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Orders_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_Ads_AdID",
+                        column: x => x.AdID,
+                        principalTable: "Ads",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Orders_Ads_AdID1",
+                        column: x => x.AdID1,
+                        principalTable: "Ads",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "reports",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReportedByID = table.Column<int>(type: "int", nullable: false),
+                    AdID = table.Column<int>(type: "int", nullable: true),
+                    AccountID = table.Column<int>(type: "int", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_reports", x => x.ID);
+                    table.CheckConstraint("CK_MyEntity_OneColumnOnly", "(AccountID IS NOT NULL AND AdID IS NULL) OR (AccountID IS NULL AND AdID IS NOT NULL)");
+                    table.ForeignKey(
+                        name: "FK_reports_Accounts_AccountID",
+                        column: x => x.AccountID,
+                        principalTable: "Accounts",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_reports_Ads_AdID",
+                        column: x => x.AdID,
+                        principalTable: "Ads",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ads_AccountID",
+                table: "Ads",
+                column: "AccountID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ads_CategoryID",
+                table: "Ads",
+                column: "CategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ads_PictureID",
+                table: "Ads",
+                column: "PictureID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Favourites_AccountID",
+                table: "Favourites",
+                column: "AccountID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Favourites_AdID",
+                table: "Favourites",
+                column: "AdID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Favourites_AdID1",
+                table: "Favourites",
+                column: "AdID1",
+                unique: true,
+                filter: "[AdID1] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_AccountId",
+                table: "Orders",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_AdID",
+                table: "Orders",
+                column: "AdID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_AdID1",
+                table: "Orders",
+                column: "AdID1",
+                unique: true,
+                filter: "[AdID1] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reports_AccountID",
+                table: "reports",
+                column: "AccountID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reports_AdID",
+                table: "reports",
+                column: "AdID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Favourites");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "reports");
+
+            migrationBuilder.DropTable(
+                name: "Ads");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropTable(
+                name: "categories");
+        }
+    }
+}
