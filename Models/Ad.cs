223acc92ef4bb2f409b1b92ee482c0be37ac9a0b
s1ggelin <80677519+s1ggelin@users.noreply.github.com>
2024-05-15 13:14:32 +0200

    added methods for adservices.cs

--- Models/Ad.cs
+++ Models/Ad.cs
@@ -3,26 +3,26 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FogelFormedlingenAB.Models
 {
-    public class Ad
-    {
-        public int ID { get; set; }
+	public class Ad
+	{
+		public int ID { get; set; }
 
-        public int AccountID { get; set; } // AccountID
+		public int AccountID { get; set; }
 		public Account Account { get; set; }
-        public int ImageID { get; set; }
-        public Image Image { get; set; }
-        public int CategoryID { get; set; }
-        public Category Category { get; set; }
+		public int ImageID { get; set; }
+		public Image Image { get; set; }
+		public int CategoryID { get; set; }
+		public Category Category { get; set; }
 		[MaxLength(80)]
 		public required string Title { get; set; }
 		[MaxLength(500)]
 		public required string Description { get; set; }
-        [Column(TypeName = "money")]
-        public decimal Price { get; set; }
-        public bool IsActive { get; set; } = true;
+		[Column(TypeName = "money")]
+		public decimal Price { get; set; }
+		public bool IsActive { get; set; } = true;
 		public DateTime StartDate { get; set; }
 
-        public Order? Order { get; set; }
-        public Favourite? Favourite { get; set; }
-    }
+		public Order? Order { get; set; }
+		public Favourite? Favourite { get; set; }
+	}
 }
--- Services/AdServices.cs
+++ Services/AdServices.cs
@@ -1,8 +1,4 @@
 ﻿using FogelFormedlingenAB.Models;
-using FogelFormedlingenAB.Controllers;
-using FogelFormedlingenAB.Data;
-using System.Text.Json;
-using System.Net.Http;
 using Newtonsoft.Json;
 
 
@@ -14,7 +10,7 @@ namespace FogelFormedlingenAB.Services
 	{
 		private readonly HttpClient _httpClient;
 
-		
+
 		public AdServices(HttpClient httpClient)
 		{
 			_httpClient = httpClient;
@@ -37,7 +33,7 @@ namespace FogelFormedlingenAB.Services
 					{
 						string responseBody = await response.Content.ReadAsStringAsync();
 
-						
+
 						ads = JsonConvert.DeserializeObject<List<Ad>>(responseBody);
 					}
 					else
@@ -52,6 +48,127 @@ namespace FogelFormedlingenAB.Services
 			}
 
 			return ads;
+		}
+		public static async Task<Ad> GetAd(int adId)
+		{
+			Ad ad = null;
+
+			using (HttpClient client = new HttpClient())
+			{
+				try
+				{
+					string baseUrl = "https://localhost:5000/";
+					string endpoint = $"api/ads/{adId}";
+					string fullUrl = baseUrl + endpoint;
+
+					HttpResponseMessage response = await client.GetAsync(fullUrl);
+
+					if (response.IsSuccessStatusCode)
+					{
+						string responseBody = await response.Content.ReadAsStringAsync();
+
+
+						ad = JsonConvert.DeserializeObject<Ad>(responseBody);
+					}
+					else
+					{
+						Console.WriteLine($"Error: {response.StatusCode}");
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"An error occurred: {ex.Message}");
+				}
+				return ad;
+			}
+		}
+		public static async Task<bool> CreateAd(Ad newAd)
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				try
+				{
+					string baseUrl = "https://localhost:5000/";
+					string endpoint = "ads";
+					string fullUrl = baseUrl + endpoint;
+
+					var json = JsonConvert.SerializeObject(newAd);
+					var data = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+					HttpResponseMessage respone = await client.PostAsync(fullUrl, data);
+
+					if (respone.IsSuccessStatusCode)
+					{
+						return true;
+					}
+					else
+					{
+						Console.WriteLine($"Error: {respone.StatusCode}");
+						return false;
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"An error occurred: {ex.Message}");
+					return false;
+				}
+			}
+		}
+		public static async Task<bool> UpdateAd(int adId, Ad updatedAd)
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				try
+				{
+					string baseUrl = "https://localhost:5000/";
+					string endpoint = $"api/ad/{adId}";
+					string fullUrl = baseUrl + endpoint;
+					var json = JsonConvert.SerializeObject(updatedAd);
+					var data = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+					HttpResponseMessage response = await client.PutAsync(fullUrl, data);
+					if (response.IsSuccessStatusCode)
+					{
+						return true;
+					}
+					else
+					{
+						Console.WriteLine($"Error: {response.StatusCode}");
+						return false;
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"An error occurred: {ex.Message}");
+					return false;
+				}
+			}
+		}
+		public static async Task<bool> DeleteAd(int adId)
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				try
+				{
+					string baseUrl = "https://localhost:5000/";
+					string endpoint = $"api/ad/{adId}";
+					string fullUrl = baseUrl + endpoint;
+					HttpResponseMessage response = await client.DeleteAsync(fullUrl);
+					if (response.IsSuccessStatusCode)
+					{
+						return true;
+					}
+					else
+					{
+						Console.WriteLine($"Error: {response.StatusCode}");
+						return false;
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"An error occurred: {ex.Message}");
+					return false;
+				}
+			}
 		}
 	}
 }
