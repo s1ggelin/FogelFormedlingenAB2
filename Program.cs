d3630a5b21408b8a0d87d117796e87cb65ffd9d0
s1ggelin <80677519+s1ggelin@users.noreply.github.com>
2024-05-21 10:53:31 +0200

    added method to automatically load our sample data when we run the application

--- Program.cs
+++ Program.cs
@@ -1,68 +1,69 @@
+using FogelFormedlingenAB.Controllers;
 using FogelFormedlingenAB.Data;
 using FogelFormedlingenAB.Models;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
 
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthentication(options =>
 {
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = "Google";
+	options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+	options.DefaultChallengeScheme = "Google";
 })
 .AddCookie(options =>
 {
-    // When a user logs in to Google for the first time, create a local account for that user in our database.
-    options.Events.OnValidatePrincipal += async context =>
-    {
-        var serviceProvider = context.HttpContext.RequestServices;
-        using var db = new AppDbContext(serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>());
+	// When a user logs in to Google for the first time, create a local account for that user in our database.
+	options.Events.OnValidatePrincipal += async context =>
+	{
+		var serviceProvider = context.HttpContext.RequestServices;
+		using var db = new AppDbContext(serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>());
 
-        string subject = context.Principal.FindFirstValue(ClaimTypes.NameIdentifier);
-        string issuer = context.Principal.FindFirst(ClaimTypes.NameIdentifier).Issuer;
-        string name = context.Principal.FindFirst(ClaimTypes.Name)?.Value;
-        if (name.IsNullOrEmpty())
-        {
-            name = context.Principal.FindFirst("name").Value;
-        }
+		string subject = context.Principal.FindFirstValue(ClaimTypes.NameIdentifier);
+		string issuer = context.Principal.FindFirst(ClaimTypes.NameIdentifier).Issuer;
+		string name = context.Principal.FindFirst(ClaimTypes.Name)?.Value;
+		if (name.IsNullOrEmpty())
+		{
+			name = context.Principal.FindFirst("name").Value;
+		}
 
 
 
-        var account = db.Accounts
-            .FirstOrDefault(p => p.OpenIDIssuer == issuer && p.OpenIDSubject == subject);
+		var account = db.Accounts
+			.FirstOrDefault(p => p.OpenIDIssuer == issuer && p.OpenIDSubject == subject);
 
-        if (account == null)
-        {
-            account = new Account
-            {
-                OpenIDIssuer = issuer,
-                OpenIDSubject = subject,
-                Name = name,
-                Phonenumber = " ",
-                Email = " "
+		if (account == null)
+		{
+			account = new Account
+			{
+				OpenIDIssuer = issuer,
+				OpenIDSubject = subject,
+				Name = name,
+				Phonenumber = " ",
+				Email = " "
 
-            };
-            db.Accounts.Add(account);
-        }
-        else
-        {
-            // If the account already exists, just update the name in case it has changed. asdasd
-            account.Name = name;
-        }
+			};
+			db.Accounts.Add(account);
+		}
+		else
+		{
+			// If the account already exists, just update the name in case it has changed. asdasd
+			account.Name = name;
+		}
 
-        await db.SaveChangesAsync();
-    };
+		await db.SaveChangesAsync();
+	};
 })
 .AddOpenIdConnect("Google", options =>
 {
-    options.Authority = "https://accounts.google.com";
-    /*
+	options.Authority = "https://accounts.google.com";
+	/*
     These two values (client ID and client secret) must be created in the Google Cloud Platform Console:
     https://support.google.com/cloud/answer/6158849?hl=en
     They must then be added to the project's "user secrets": right-click the project in Visual Studio and select "Manage User Secrets" and write the following JSON:
@@ -75,29 +76,29 @@ builder.Services.AddAuthentication(options =>
        }
     }
     */
-    options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
-    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
-    options.ResponseType = OpenIdConnectResponseType.Code;
-    options.CallbackPath = "/signin-oidc-google";
-    options.SignedOutCallbackPath = "/signout-callback-oidc-google";
-    options.Scope.Add("openid");
-    options.Scope.Add("profile");
-    options.Scope.Add("email");
-    options.SaveTokens = true;
-    options.GetClaimsFromUserInfoEndpoint = true;
-    options.Events.OnSignedOutCallbackRedirect = context =>
-    {
-        context.Response.Redirect("/Index");
-        context.HandleResponse();
-        return Task.CompletedTask;
-    };
+	options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+	options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+	options.ResponseType = OpenIdConnectResponseType.Code;
+	options.CallbackPath = "/signin-oidc-google";
+	options.SignedOutCallbackPath = "/signout-callback-oidc-google";
+	options.Scope.Add("openid");
+	options.Scope.Add("profile");
+	options.Scope.Add("email");
+	options.SaveTokens = true;
+	options.GetClaimsFromUserInfoEndpoint = true;
+	options.Events.OnSignedOutCallbackRedirect = context =>
+	{
+		context.Response.Redirect("/Index");
+		context.HandleResponse();
+		return Task.CompletedTask;
+	};
 });
 
 builder.Services.AddAuthorization(options =>
 {
-    options.FallbackPolicy = new AuthorizationPolicyBuilder()
-        .RequireAuthenticatedUser()
-        .Build();
+	options.FallbackPolicy = new AuthorizationPolicyBuilder()
+		.RequireAuthenticatedUser()
+		.Build();
 });
 
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
@@ -110,18 +111,24 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<AccessControl>();
 
+builder.Services.AddScoped<APIController>();
+builder.Services.AddLogging(logging =>
+{
+	logging.AddConsole();
+});
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	app.UseHsts();
 }
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -136,11 +143,15 @@ app.MapControllers();
 
 using (var scope = app.Services.CreateScope())
 {
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<AppDbContext>();
+	var services = scope.ServiceProvider;
+	var context = services.GetRequiredService<AppDbContext>();
 
-    SampleData.CreateCategorys(context);
-    SampleData.CreateAccounts(context);
+	SampleData.CreateCategorys(context);
+	SampleData.CreateAccounts(context);
+
+	// Method to automatically load SampleData when we run our application
+	var apiController = services.GetRequiredService<APIController>();
+	await apiController.AddSampleData();
 }
 
 app.Run();
